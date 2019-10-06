767d4e0f78dce677813667f6330f7385f48c278c
Andrei Dobrinski <andreiidobrinski@gmail.com>
2019-10-06 18:43:17 -0400

    remove demo app

---
+++ lib/src/app.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class App extends StatelessWidget {
+  Widget build(context) {
+    return MaterialApp(
+      title: 'Login',
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('todo app'),
+        ),
+        body: Text('todo app body'),
+      ),
+    );
+  }
+}
