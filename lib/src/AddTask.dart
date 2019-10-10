a42b9c1f3d1aabcb38433bf49a4d1191ba2250f7
Andrei Dobrinski <andreiidobrinski@gmail.com>
2019-10-09 23:27:34 -0400

    add button to handle TextField state

--- lib/src/AddTask.dart
+++ lib/src/AddTask.dart
@@ -11,6 +11,7 @@ class AddTask extends StatefulWidget {
 
 class AddTaskState extends State<AddTask> {
   final taskTitleController = TextEditingController();
+  bool showTextField = false;
 
   @override
   void dispose() {
@@ -26,20 +27,47 @@ class AddTaskState extends State<AddTask> {
       );
       Provider.of<TodoProvider>(context, listen: false).addTodo(todo);
       taskTitleController.clear();
+      setState(() {
+        showTextField = false;
+      });
     }
   }
 
   @override
   Widget build(BuildContext context) {
+    if (!showTextField) {
+      return RaisedButton(
+          child: Text(
+            'Add New Todo',
+            style: TextStyle(
+              color: Colors.white,
+            ),
+          ),
+          color: Colors.blue,
+          onPressed: () {
+            setState(() {
+              showTextField = true;
+            });
+          });
+    }
     return Padding(
       padding: EdgeInsets.all(15.0),
       child: Container(
         child: Column(
           crossAxisAlignment: CrossAxisAlignment.stretch,
           children: <Widget>[
-            TextField(controller: taskTitleController),
+            TextField(
+              controller: taskTitleController,
+              autofocus: true,
+            ),
             RaisedButton(
-              child: Text('Add'),
+              child: Text(
+                'Add',
+                style: TextStyle(
+                  color: Colors.white,
+                ),
+              ),
+              color: Colors.blue,
               onPressed: onAdd,
             ),
           ],
